759f4ce700f99e331680d36b1f8a7b7558e0755a
Shadab Khan <shadab.2005@gmail.com>
2022-11-21 14:06:39 +0530

    add deployment script to deploy contracts and mint tokens

--- contracts/ERC20Votes.sol
+++ contracts/ERC20Votes.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 contract MyToken is ERC20, AccessControl, ERC20Permit, ERC20Votes {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {
+    constructor() ERC20("ESBCGroup10", "GRTEN") ERC20Permit("MyToken") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
     }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   paths: { tests: "tests" },
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      accounts: [process.env.SOMETHING_IMPORTANT as string],
+    },
+  },
 };
 
 export default config;
