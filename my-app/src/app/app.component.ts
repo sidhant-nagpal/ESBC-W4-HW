1e25d36251b4c704d6b71343f4c522d7fc910665
Yash Gadhade <yashgadhade@gmail.com>
2022-11-26 12:27:30 +0530

    tokenpush

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ethers } from 'ethers';
+import tokenJson from '../assets/MyToken.json';
+
+const TOKENIZED_VOTES_ADDRESS = '0xF6e8FF785bA56C14DeE6056d457e2F78DB868Ece';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,10 @@ import { ethers } from 'ethers';
 export class AppComponent {
   provider: ethers.providers.Provider;
   wallet: ethers.Wallet | undefined;
+  tokenContract: ethers.Contract | undefined;
+  etherBalance: number | undefined;
+  tokenBalance: number | undefined;
+  votePower: number | undefined;
 
   constructor() {
     this.provider = ethers.providers.getDefaultProvider('goerli');
@@ -16,5 +23,23 @@ export class AppComponent {
 
   createWallet() {
     this.wallet = ethers.Wallet.createRandom().connect(this.provider);
+    this.tokenContract = new ethers.Contract(
+      TOKENIZED_VOTES_ADDRESS,
+      tokenJson.abi,
+      this.wallet
+    );
+    this.wallet.getBalance().then((balanceBN: ethers.BigNumberish) => {
+      this.etherBalance = parseFloat(ethers.utils.formatEther(balanceBN));
+    });
+    this.tokenContract['balanceOf'](this.wallet.address).then(
+      (balanceBN: ethers.BigNumberish) => {
+        this.etherBalance = parseFloat(ethers.utils.formatEther(balanceBN));
+      }
+    );
+    this.tokenContract['getVotes'](this.wallet.address).then(
+      (votesBN: ethers.BigNumberish) => {
+        this.votePower = parseFloat(ethers.utils.formatEther(votesBN));
+      }
+    );
   }
 }
